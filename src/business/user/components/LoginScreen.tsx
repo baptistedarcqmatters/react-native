c844b721a9de414e0a9dee5583e8bad5506a2c53
Baptiste Darcq <baptiste.darcq@matters.tech>
2019-10-18 18:00:42 +0200

    make a change

--- src/business/user/components/LoginScreen.tsx
+++ src/business/user/components/LoginScreen.tsx
@@ -1,22 +1,24 @@
-import React, { useCallback } from "react";
-import { NavigationScreenProps } from "react-navigation";
-import translator from "src/i18n";
-import Button from "ui/buttons";
-import Layout from "ui/layout";
+import React, { useCallback } from 'react';
+import { NavigationScreenProps } from 'react-navigation';
+import translator from 'src/i18n';
+import Button from 'ui/buttons';
+import Layout from 'ui/layout';
+import Text from 'ui/typo/text';
 
 interface Props extends NavigationScreenProps {}
 
 const LoginScreen: React.SFC<Props> = ({ navigation }) => {
   const requestLogin = useCallback(() => {
-    navigation.navigate("Home");
+    navigation.navigate('Home');
   }, []);
 
   return (
     <Layout.Page>
       <Button.Primary
-        label={translator.t("login.cta")}
+        label={translator.t('login.cta')}
         onPress={requestLogin}
       />
+      <Text style={{ color: '#FF6600' }}>Tu dis plus rien la</Text>
     </Layout.Page>
   );
 };
